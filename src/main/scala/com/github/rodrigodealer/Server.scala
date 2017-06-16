879732518ab25b267586c65775507a852bebcbaf
Rodrigo Oliveira <rodrigo.dealer@gmail.com>
2017-06-16 21:30:45 +0200

    Stuff

--- src/main/scala/com/github/rodrigodealer/Server.scala
+++ src/main/scala/com/github/rodrigodealer/Server.scala
@@ -1,63 +1,16 @@
-import java.util.UUID
-
-import com.github.rodrigodealer.Todo
+import com.github.rodrigodealer.TodoController
 import com.twitter.app.Flag
 import com.twitter.finagle.{Http, Service}
 import com.twitter.finagle.http.{Request, Response}
-import com.twitter.finagle.stats.Counter
 import com.twitter.server.TwitterServer
 import com.twitter.util.Await
 import io.circe.generic.auto._
 import io.finch._
 import io.finch.circe._
 
-object Main extends TwitterServer {
+object Main extends TwitterServer with TodoController {
 
   val port: Flag[Int] = flag("port", 8081, "TCP port for HTTP server")
-
-  val todos: Counter = statsReceiver.counter("todos")
-
-  def postedTodo: Endpoint[Todo] = jsonBody[UUID => Todo].map(_(UUID.randomUUID()))
-
-  def postTodo: Endpoint[Todo] = post("todos" :: postedTodo) { t: Todo =>
-    todos.incr()
-    Todo.save(t)
-
-    Created(t)
-  }
-
-  def patchedTodo: Endpoint[Todo => Todo] = jsonBody[Todo => Todo]
-
-  def patchTodo: Endpoint[Todo] =
-    patch("todos" :: path[UUID] :: patchedTodo) { (id: UUID, pt: Todo => Todo) =>
-      Todo.get(id) match {
-        case Some(currentTodo) =>
-          val newTodo: Todo = pt(currentTodo)
-          Todo.delete(id)
-          Todo.save(newTodo)
-
-          Ok(newTodo)
-        case None => throw new IllegalStateException("Not found")
-      }
-    }
-
-  def getTodos: Endpoint[List[Todo]] = get("todos") {
-    Ok(Todo.list())
-  }
-
-  def deleteTodo: Endpoint[Todo] = delete("todos" :: path[UUID]) { id: UUID =>
-    Todo.get(id) match {
-      case Some(t) => Todo.delete(id); Ok(t)
-      case None => throw new IllegalStateException("Not found")
-    }
-  }
-
-  def deleteTodos: Endpoint[List[Todo]] = delete("todos") {
-    val all: List[Todo] = Todo.list()
-    all.foreach(t => Todo.delete(t.id))
-
-    Ok(all)
-  }
 
   val api: Service[Request, Response] = (
     getTodos :+: postTodo :+: deleteTodo :+: deleteTodos :+: patchTodo
---
+++ src/main/scala/com/github/rodrigodealer/TodoController.scala
@@ -0,0 +1,50 @@
+package com.github.rodrigodealer
+
+import java.util.UUID
+
+import io.finch.{Created, Endpoint, Ok, delete, get, jsonBody, patch, path, post}
+
+trait TodoController {
+
+  def postedTodo: Endpoint[Todo] = jsonBody[UUID => Todo].map(_(UUID.randomUUID()))
+
+  def postTodo: Endpoint[Todo] = post("todos" :: postedTodo) { t: Todo =>
+    Todo.save(t)
+
+    Created(t)
+  }
+
+  def patchedTodo: Endpoint[Todo => Todo] = jsonBody[Todo => Todo]
+
+  def patchTodo: Endpoint[Todo] =
+    patch("todos" :: path[UUID] :: patchedTodo) { (id: UUID, pt: Todo => Todo) =>
+      Todo.get(id) match {
+        case Some(currentTodo) =>
+          val newTodo: Todo = pt(currentTodo)
+          Todo.delete(id)
+          Todo.save(newTodo)
+
+          Ok(newTodo)
+        case None => throw new IllegalStateException("Not found")
+      }
+    }
+
+  def getTodos: Endpoint[List[Todo]] = get("todos") {
+    Ok(Todo.list())
+  }
+
+  def deleteTodo: Endpoint[Todo] = delete("todos" :: path[UUID]) { id: UUID =>
+    Todo.get(id) match {
+      case Some(t) => Todo.delete(id); Ok(t)
+      case None => throw new IllegalStateException("Not found")
+    }
+  }
+
+  def deleteTodos: Endpoint[List[Todo]] = delete("todos") {
+    val all: List[Todo] = Todo.list()
+    all.foreach(t => Todo.delete(t.id))
+
+    Ok(all)
+  }
+
+}
